b34172389c44d33d9ca75fa9bf410d99e9c67c25
Joseph Price <josephprice747@gmail.com>
2023-01-24 17:29:23 +0000

    add Q6, add quizScore and final tally

--- app.js
+++ app.js
@@ -5,18 +5,20 @@ function getName() {
   alert(
     "Welcome to my about me website " +
       userName +
-      "! BUT before we get to the page I'd like to play a little game with you. Please answer yes or no to the following:"
+      " ! BUT before we get to the page I'd like to play a little game with you. Please answer yes or no to the following:"
   );
   document.write(userName);
   console.log(userName);
 }
 
 function quiz() {
+  let quizScore = 0;
   let question1 = prompt("Do I have a dog?");
   console.log(question1);
 
   if (question1.toLowerCase() === "yes") {
     alert("You're absolutely right I do, she's a good girl");
+    quizScore++;
   } else if (question1.toLowerCase() === "no") {
     alert("I'm afraid you're wrong :( Sorry");
   } else {
@@ -30,6 +32,7 @@ function quiz() {
     alert("Nope, but hopefully that'll change soon");
   } else if (question2.toLowerCase() === "no") {
     alert("That's right, it's quite boring");
+    quizScore++;
   } else {
     alert("That's not a real answer");
   }
@@ -43,6 +46,7 @@ function quiz() {
     alert(
       "Correctamundo, I've had driving lessons and passed the theory test but I never went further than that"
     );
+    quizScore++;
   } else {
     alert("That's not a real answer");
   }
@@ -52,6 +56,7 @@ function quiz() {
 
   if (question4.toLowerCase() === "yes") {
     alert("You know me so well :) I should cut down on it");
+    quizScore++;
   } else if (question4.toLowerCase() === "no") {
     alert("You've clearly never met me, you're wrong my friend");
   } else {
@@ -63,11 +68,13 @@ function quiz() {
 
   if (question5.toLowerCase() === "yes") {
     alert("Yep yep yep! Gaming is my biggest hobby");
+    quizScore++;
   } else if (question5.toLowerCase() === "no") {
     alert("Are you playing games with me? You've got that one wrong buddy");
   } else {
     alert("That's not a real answer");
   }
+  alert("You've reached the end, well done! You got " + quizScore + "/5");
 }
 
 function changeColour() {
